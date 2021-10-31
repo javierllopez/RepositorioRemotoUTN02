4728772c8ecb0a95375239a38f3a4fc156d849b7
Javier Lopez <javier_l@yahoo.com>
2021-10-31 18:24:53 -0300

    Trabajo práctico finalizado

--- ProyectoNodeJs/app.js
+++ ProyectoNodeJs/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 require('dotenv').config();
 
@@ -16,6 +17,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//Express-sessions
+app.use(session({
+  secret:'jf84jjdj882jA!9&ehH5e5f5g5ty4h',
+  resave:false,
+  saveUninitialized:true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
